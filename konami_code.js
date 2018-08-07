c7c6aba7ec5d657c9a149645fb3e94ce3f9740ba
Nicolas Travis <home@Nicolass-MacBook-Pro.local>
2018-08-07 17:39:27 +0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let i = 0;
+  document.addEventListener('keydown', function(key) {
+	  	
+	  	if (key.which === code[i]) {
+	  		i++;
+	  		if(i === code.length){
+	  			window.alert("wefwefqwefqwf");
+	  			i = 0;
+	  		}
+	  		
+	  		
+	  	} else {
+	  		i = 0;
+	  	}
+	 
+	});
 }
+
+init();
